bbb7857f198445d54c9dd324f73615ce234a83ab
EiPhyu15 <thineiphyu1561998@gmail.com>
2025-09-08 12:59:56 +0800

    CorrectError

--- QRbasedFoodOrdering/Controllers/RolesAdminController.cs
+++ QRbasedFoodOrdering/Controllers/RolesAdminController.cs
@@ -12,7 +12,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace QRbasedFoodOrdering.Controllers
 {
-   [Authorize(Roles ="Admin")]
+  // [Authorize(Roles ="Admin")]
     public class RolesAdminController : Controller
     {
         private readonly ApplicationDbContext _context;
